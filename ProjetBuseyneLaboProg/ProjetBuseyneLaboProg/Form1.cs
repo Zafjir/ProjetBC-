179e8c6cc0bf39d25924d70210a121aff4c6dc09
salah-dine ajellabi <sasa199401@hotmail.com>
2017-02-09 16:33:54 +0100

    mod

--- ProjetBuseyneLaboProg/ProjetBuseyneLaboProg/Form1.cs
+++ ProjetBuseyneLaboProg/ProjetBuseyneLaboProg/Form1.cs
@@ -16,5 +16,10 @@ namespace ProjetBuseyneLaboProg
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
