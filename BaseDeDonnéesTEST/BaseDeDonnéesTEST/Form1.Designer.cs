87630ca46d802b507d88761cd21775928eef7e1c
Zaf-Portable <thomas.berte25@gmail.com>
2017-03-14 09:29:27 +0100

    BDD test

---
+++ BaseDeDonnéesTEST/BaseDeDonnéesTEST/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace BaseDeDonnéesTEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sync";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(104, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 199);
+            this.listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(152, 42);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 199);
+            this.listBox2.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 253);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BaseDeDonnéesTEST/BaseDeDonnéesTEST/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace BaseDeDonnéesTEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Class1.conn = new OleDbConnection();
+            Class1.cmd = new OleDbCommand(); 
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1.conn.ConnectionString = Properties.Settings.Default.OledbConnectionString2010;
+            try
+            {
+                Class1.conn.Open();
+                if (Class1.conn.State==ConnectionState.Open)
+                {
+                    //MessageBox.Show("Connection established");
+                    string sqlstr, enr;
+
+                    sqlstr = "select Nom from TailleDeBite";
+                    Class1.cmd.CommandType = CommandType.Text;
+                    Class1.cmd.CommandText = sqlstr;
+                    Class1.cmd.Connection = Class1.conn;
+                    Class1.dtrd = Class1.cmd.ExecuteReader();
+                    listBox1.Items.Clear();
+                    while (Class1.dtrd.Read())
+                    {
+                        enr = Class1.dtrd["Nom"].ToString();
+                        listBox1.Items.Add(enr);
+                    }
+                    if (Class1.dtrd != null)
+                    {
+                        Class1.dtrd.Close();
+                    }
+
+                    sqlstr = "select Taille from TailleDeBite";
+                    Class1.cmd.CommandType = CommandType.Text;
+                    Class1.cmd.CommandText = sqlstr;
+                    Class1.cmd.Connection = Class1.conn;
+                    Class1.dtrd = Class1.cmd.ExecuteReader();
+                    listBox2.Items.Clear();
+                    while (Class1.dtrd.Read())
+                    {
+                        enr = Class1.dtrd["Taille"].ToString();
+                        listBox2.Items.Add(enr);
+                    }
+                    if (Class1.dtrd != null)
+                    {
+                        Class1.dtrd.Close();
+                    }
+
+                    if (Class1.conn.State==ConnectionState.Open)
+                    {
+                        Class1.conn.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Connection failed");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Class1.conn.ConnectionString = Properties.Settings.Default.OledbConnectionString2010;
+            try
+            {
+                Class1.conn.Open();
+                if (Class1.conn.State == ConnectionState.Open)
+                {
+                    //MessageBox.Show("Connection established");
+                    string sqlstr, enr;
+
+                    sqlstr = "select Nom from TailleDeBite";
+                    Class1.cmd.CommandType = CommandType.Text;
+                    Class1.cmd.CommandText = sqlstr;
+                    Class1.cmd.Connection = Class1.conn;
+                    Class1.dtrd = Class1.cmd.ExecuteReader();
+                    listBox1.Items.Clear();
+                    while (Class1.dtrd.Read())
+                    {
+                        enr = Class1.dtrd["Nom"].ToString();
+                        listBox1.Items.Add(enr);
+                    }
+                    if (Class1.dtrd != null)
+                    {
+                        Class1.dtrd.Close();
+                    }
+
+                    sqlstr = "select Taille from TailleDeBite";
+                    Class1.cmd.CommandType = CommandType.Text;
+                    Class1.cmd.CommandText = sqlstr;
+                    Class1.cmd.Connection = Class1.conn;
+                    Class1.dtrd = Class1.cmd.ExecuteReader();
+                    listBox2.Items.Clear();
+                    while (Class1.dtrd.Read())
+                    {
+                        enr = Class1.dtrd["Taille"].ToString();
+                        listBox2.Items.Add(enr);
+                    }
+                    if (Class1.dtrd != null)
+                    {
+                        Class1.dtrd.Close();
+                    }
+
+                    if (Class1.conn.State == ConnectionState.Open)
+                    {
+                        Class1.conn.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Connection failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
